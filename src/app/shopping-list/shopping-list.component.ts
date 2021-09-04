8539b262a336fc7196599084a6800ce90657d92d
kucse160235 <afnan1635@cseku.ac.bd>
2021-09-04 14:29:38 +0600

    Addition of Header file

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingrediant } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+
+  ingredients: Ingrediant[] = [
+    new Ingrediant("Apples", 5),
+    new Ingrediant("Tomatos", 10)
+  ];
 
   constructor() { }
 
