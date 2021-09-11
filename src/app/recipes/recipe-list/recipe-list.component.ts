a15459a9106d149dc17a5f6111159ade84ebcdf0
kucse160235 <afnan1635@cseku.ac.bd>
2021-09-12 00:46:07 +0600

    RecipeList and RecipeDetails component connection successful

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,14 +7,20 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() onRecipeItemSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
-    new Recipe("Khankir Pola", "This is a khanki Dish.. Please do me a khanki", "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=768,574"),
-    new Recipe("Khankir Pola", "This is a khanki Dish.. Please do me a khanki", "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=768,574"),
-    new Recipe("Khankir Pola", "This is a khanki Dish.. Please do me a khanki", "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=768,574")
+    new Recipe("Polaw", "This is a nice Dish.. Please try.", "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=768,574"),
+    new Recipe("Vaat", "This is a nice Dish.. Please try.", "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=768,574"),
+    new Recipe("Biriani", "This is a nice Dish.. Please try.", "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=768,574")
   ];
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe){
+    this.onRecipeItemSelected.emit(recipe);
   }
 
 }
